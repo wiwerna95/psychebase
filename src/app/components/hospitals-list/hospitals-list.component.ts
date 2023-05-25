2d42b4a525662ea8db7ed67ec26ef3c49410e4d2
Ewelina Glapiak <ewelina.glapiak@forcom.com.pl>
2023-05-25 11:47:34 +0200

    problemiki 3

---
+++ src/app/components/hospitals-list/hospitals-list.component.ts
@@ -0,0 +1,31 @@
+import { Hospital } from 'src/app/models/Hospital.model';
+import { HospitalService } from '../../services/hospital.service';
+import { Component, OnInit } from '@angular/core';
+import { map, subscribeOn } from 'rxjs';
+import { ActivatedRoute, Route, RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-hospitals-list',
+  templateUrl: './hospitals-list.component.html',
+  styleUrls: ['./hospitals-list.component.css']
+})
+export class HospitalsListComponent implements OnInit {
+  hospitals: Hospital[] = [];
+
+  constructor(private hospitalService: HospitalService, private router: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.hospitalService.getAll()
+    .subscribe( (data: any) => {
+      data.docs.forEach((doc: any) => {
+        this.hospitals.push(doc.data())
+      })
+    })
+    console.log(this.hospitals)
+  }
+}
